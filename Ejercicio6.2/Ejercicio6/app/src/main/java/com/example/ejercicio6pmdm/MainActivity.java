3a092593a27301abc569f6c7ce3e5a3bc42493b5
Carlos <infocarlosexpuig@gmail.com>
2022-11-17 19:19:07 +0100

    Ejercicio 6 no terminado

--- Ejercicio6.2/Ejercicio6/app/src/main/java/com/example/ejercicio6pmdm/MainActivity.java
+++ Ejercicio6.2/Ejercicio6/app/src/main/java/com/example/ejercicio6pmdm/MainActivity.java
@@ -4,47 +4,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.example.ejercicio6pmdm.databinding.ActivityBottomBinding;
+import com.example.ejercicio6pmdm.databinding.ActivityDrawerBinding;
 import com.example.ejercicio6pmdm.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
-
+    ActivityDrawerBinding binding1;
+    ActivityBottomBinding binding2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+        //Drawer
+        setContentView((binding1 = ActivityDrawerBinding.inflate(getLayoutInflater())).getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        setSupportActionBar(binding1.toolbar);
 
-        binding.actionGotoDrawerActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, DrawerActivity.class));
-            }
-        });
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                // Top-level destinations:
+                R.id.drawer1Fragment, R.id.drawer2Fragment
+        )
+                .setOpenableLayout(binding1.drawerLayout)
+                .build();
 
-        binding.actionGotoBottomActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, BottomActivity.class));
-            }
-        });
+        NavController navController1 = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding1.navView, navController1);
+        NavigationUI.setupWithNavController(binding1.toolbar, navController1, appBarConfiguration);
+        //Bottom
+        setContentView((binding2 = ActivityBottomBinding.inflate(getLayoutInflater())).getRoot());
 
-        binding.actionGotoOptionsActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, OptionsActivity.class));
-            }
-        });
+        setSupportActionBar(binding2.toolbar);
 
-        binding.actionGotoTabbedActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, TabbedActivity.class));
-            }
-        });
+        NavController navController2 = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding2.bottomNavView, navController2);
+        NavigationUI.setupWithNavController(binding2.toolbar, navController2);
     }
 }
+
+
